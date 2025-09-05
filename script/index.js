ec62786bd4f227fe6de4fe2968739df1659dbc28
akib bhuiyan <akibbhuiyan3544@gmail.com>
2025-09-05 11:08:26 +0600

    aded more js fetures

--- script/index.js
+++ script/index.js
@@ -1,10 +1,25 @@
+const spinmanage = (status) => {
+    if (status === true) {
+        document.getElementById("loding").classList.remove("hidden");
+        // document.getElementById("loding").classList.remove("hidden");
+        document.getElementById("words-continer").classList.add("hidden");
+    } else {
+        document.getElementById("loding").classList.add("hidden");
+        document.getElementById("words-continer").classList.remove("hidden");
+    }
+}
+
 const getlabel = () => {
+    
+    
     fetch("https://openapi.programming-hero.com/api/levels/all") //it return promice and waiting for provide data
         .then(res => res.json())
         .then(datas => displayleson(datas.data))
 }
 
 const getwords = (id) => {
+    
+    spinmanage(true)
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
 
     fetch(url)
@@ -13,7 +28,7 @@ const getwords = (id) => {
 
 }
 
-const loadWordDetail = async(id) =>{
+const loadWordDetail = async (id) => {
     const url = `https://openapi.programming-hero.com/api/word/${id}`;
     console.log(url);
     const res = await fetch(url);
@@ -24,6 +39,7 @@ const loadWordDetail = async(id) =>{
 }
 
 const displayloadWordDetail = (word) => {
+
     const modalBox = document.getElementById("modal-box");
     modalBox.innerHTML = `
         <div id="detils_box" class="space-y-5 bg-white p-4 rounded-xl">
@@ -41,9 +57,9 @@ const displayloadWordDetail = (word) => {
             </div>
             <div>
                 <h3 class="font-bold">সমার্থক শব্দ গুলো</h3>
-                ${(word.synonyms && word.synonyms.length > 0) 
-                    ? word.synonyms.map(s => `<span class="btn">${s}</span>`).join('')
-                    : `<p class="text-gray-500">কোনো সমার্থক শব্দ নাই</p>`}
+                ${(word.synonyms && word.synonyms.length > 0)
+            ? word.synonyms.map(s => `<span class="btn">${s}</span>`).join('')
+            : `<p class="text-gray-500">কোনো সমার্থক শব্দ নাই</p>`}
             </div>
             <div class="modal-action">
             <form method="dialog">
@@ -51,28 +67,32 @@ const displayloadWordDetail = (word) => {
             </form>
             </div>
         </div>`;
-    
+
     document.getElementById("my_modal").showModal();
+
 };
 
 
 
 const lebelwords = (words) => {
+    spinmanage(true);
     // console.log(words);
     const worddiv = document.getElementById("words-continer");
     worddiv.innerHTML = "";
 
-    if(words.length === 0){
-        /* alert("no data found her please click next lessone"); /* it have any length or no one fine amy enlement in perticular lessone then it will be a length value  */ 
+    if (words.length === 0) {
+        /* alert("no data found her please click next lessone"); /* it have any length or no one fine amy enlement in perticular lessone then it will be a length value  */
         worddiv.innerHTML = `<div class="text-center col-span-full bg-gray-100 my-[40px] py-[20px] rounded-2xl">
                 <img src="./assets/alert-error.png" alt="" class="mx-auto">
                 <p class="text-[#79716B] text-[14px]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
                 <h2 class="text-[35px] font-bold">নেক্সট Lesson এ যান</h2>
             </div>`
+        spinmanage(false);
         return;
     }
 
     words.forEach(wordcard => {
+
         const newword_card = document.createElement("div");
         newword_card.innerHTML = `<div class="word-card  text-center bg-white p-2 ">
                 <h2 class="text-[26px] font-bold">${wordcard.word ? wordcard.word : "শব্দ পাওয়া জায়নাই"}</h2>
@@ -84,6 +104,9 @@ const lebelwords = (words) => {
                 </div>
             </div>`;
         worddiv.appendChild(newword_card);
+        spinmanage(false);
+       
+
 
     })
 
@@ -98,7 +121,9 @@ const displayleson = (lesones) => {
         const newbutton = document.createElement("button");
         newbutton.innerHTML = ` <button id="lesson-btn-${lesone.level_no}" onclick="getwords(${lesone.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>lessone-${lesone.level_no}</button>`;
         lesson.appendChild(newbutton);
+        
     })
+
 
 }
 
